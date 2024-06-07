eaa9e532ea3ea3e897130db1c6a6fc00ee7b7966
Sabashahid689 <163736077+Sabashahid689@users.noreply.github.com>
2024-06-07 18:58:09 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,24 @@
+            //class homework assignment with typescript:-
+                     // topic data types:-
+// data types basically two types ki hoti hai primitive data types and non primitive data types.
+// There are six primitive data types.
+// (1)string
+// (2)number
+// (3)boolean
+// (4)undefined
+// (5)null
+// (6)any
+//string:
+//let name1 = "my name is saba";
+//console.log(name1);
+//number:
+//let age = 17;
+//console.log(age);
+//example 2 number ki data type ki.
+//let a = 4;
+//let b = 8;
+//console.log(a+b);
+// boolean:
+//let a = 9;
+//let b = 9;
+//console.log(a == b);               // = means assigin operators   //== means 2 assigin operators  //  === assigin means data type check kerta hai
